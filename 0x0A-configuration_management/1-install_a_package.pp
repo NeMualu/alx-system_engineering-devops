b80b9e23adf75ce6b891582be802eab896a6962e
Mualu Kianisalu <mualu.kianisalu@gmail.com>
2024-03-26 15:21:36 +0000

    My first commit

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,12 @@
-# Installs puppet-lint
-
-package { 'puppet-lint':
-  ensure   => '2.1.0',
-  provider => 'gem',
+package { 'python3-pip':
+  ensure => installed,
 }
+
+exec { 'install_flask':
+  command     => '/usr/bin/pip3 install Flask==2.1.0',
+  path        => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+  environment => ['PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'],
+  unless      => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"',
+  require     => Package['python3-pip'],
+}
+
