22d9df121b9bbe3218e02be697d81610e3104ee9
Mualu Kianisalu <mualu.kianisalu@gmail.com>
2024-03-27 16:50:03 +0000

    My first commit

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,4 +1,4 @@
-# This Puppet manifest installs Flask version 2.1.0 using pip3
+# Puppet manifest to install Flask version 2.1.0 using pip3
 
 package { 'python3-pip':
   ensure => installed,
@@ -8,4 +8,6 @@ exec { 'install_flask':
   command => '/usr/bin/pip3 install Flask==2.1.0',
   path    => '/usr/bin',
   require => Package['python3-pip'],
+  creates => '/usr/local/lib/python3.8/dist-packages/Flask-2.1.0.dist-info',
 }
+
